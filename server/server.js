cb7fbf12587f97c3875f7765874b0a0f549d16b8
pi <pi>
2021-05-28 13:56:55 +0200

    added alarm sound functionality

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // Relay info: GPIO out HIGH = relay is off. GPIO out on LOW = relay is on!
-var http = require('http'); // 1 - Import Node.js 'http' module
+let http = require('http'); // 1 - Import Node.js 'http' module
 const { MODE_BCM, DIR_HIGH } = require('rpi-gpio');
-var gpio = require('rpi-gpio');
+let gpio = require('rpi-gpio');
+let player = require('play-sound')(opts = {}) 
 
 const GPIO_PIN_COFFEE_SMALL = 18; // as example
 const GPIO_PIN_COFFEE_BIG = 21; // as example
@@ -14,9 +15,11 @@ gpio.setup(GPIO_PIN_COFFEE_BIG,DIR_HIGH); // Sets GPIO pin up with start value o
 const PORT_NUMBER = 3000;
 
 let busy = false;
+let alarmRunning = false;
+//global variable to stop music
+let alarm;
 
-
-var server = http.createServer(function (req, res) {   // 2 - creating server
+let server = http.createServer(function (req, res) {   // 2 - creating server
 
 
     if (req.url == '/') { //check the URL of the current request
@@ -61,6 +64,40 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
             res.write(`<html><body><h1>Welcome to morning brew</h1>Sorry, the machine is busy</body></html>`);
             res.end();
         }
+    }
+    else if (req.url == "/alarm-on" && req.method == 'POST'){
+        if(!alarmRunning){
+            alarmRunning = true;
+            //To fix problem of speaker only playing noise: use mpg123
+            //https://askubuntu.com/questions/115369/how-to-play-mp3-files-from-the-command-line
+            player.player = 'mpg123';
+
+            alarm = player.play('./music-files/Simon Says! - one more time (feat. Devonte) [NCS Release].mp3', function(err){
+                if (err) throw err
+            })
+            res.end('Started audio');
+        }
+        else{
+            res.end('Audio already playing');
+        }
+        
+    }   
+    else if (req.url == "/alarm-off" && req.method == 'POST'){
+        if(alarmRunning){
+            if(alarm != undefined){
+                alarm.kill();
+                alarm = undefined;
+                alarmRunning = false;
+                res.end('Stopped audio');    
+            }
+            else{
+                res.end('Audio killed by timeout')
+            }
+        }
+        else{
+            res.end('No audio playing')
+        }
+
     }
     else
         res.end('Invalid Request!');
